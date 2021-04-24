21a1d31a348120368657d07574640825b3de891c
harsh <hs98215479@gmail.com>
2021-04-24 21:27:45 +0530

    Added some test files.

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"harsh";
+	int i=0;
+	label:
+	if(i<10)
+{	
+	cout<<i<<" ";
+	i++;
+	goto label;
+}
+	
+}
---
+++ testfile.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	cout<<n;
+	cout<<"f1";
+
+
+return 0;
+}
